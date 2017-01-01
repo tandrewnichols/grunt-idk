1e28a616e4d41b7693daa3ff0adaa064613d0aa9
Andrew Nichols <tandrewnichols@gmail.com>
2016-12-31 22:36:32 -0500

    It works . . . but it's gonna suck to test

--- tasks/idk.js
+++ tasks/idk.js
@@ -1,4 +1,81 @@
-module.exports = function(grunt) {
-  grunt.registerMultiTask('idk', require('../package').description, function() {
+const inquirer = require('inquirer');
+inquirer.registerPrompt('ordered', require('inquirer-orderedcheckbox'));
+const _ = require('lodash');
+const chalk = require('chalk');
+const strip = require('strip-ansi');
+const async = require('async');
+const DEFAULT_PAGE_SIZE = 20;
+const DEFAULT_PAGE_OFFSET = 5;
+
+const self = module.exports = function(grunt) {
+  grunt.registerTask('idk', require('../package').description, function() {
+    // Tell grunt to wait for the task to complete
+    var done = this.async();
+    grunt.log.write('\033[0;0H\033[2J');
+    var options = this.options({
+      size: DEFAULT_PAGE_SIZE,
+      offset: DEFAULT_PAGE_OFFSET
+    });
+
+    // Make tasks an array
+    var tasks = _.chain(grunt.task._tasks).reduce((memo, val, task) => {
+      val.choice = `${chalk.gray(task)}${chalk.gray(':')} ${val.info}`;
+      memo.push(val);
+      return memo;
+    }, []).sortBy('name').value();
+
+    var calcSize = list => {
+      var pageSize = list.length > options.size ? options.size : list.length;
+      if (process.env.LINES && pageSize > process.env.LINES) {
+        pageSize = process.env.LINES - options.offset;
+      }
+      return pageSize;
+    };
+
+    inquirer.prompt([{
+      type: 'checkbox',
+      message: 'Select task',
+      name: 'tasks',
+      choices: _.map(tasks, 'choice'),
+      pageSize: calcSize(tasks)
+    }]).then(answers => {
+      answers = _.map(answers.tasks, task => strip(task).split(':')[0]);
+      async.mapSeries(answers, function(answer, next) {
+        var task = _.find(tasks, { name: answer });
+        if (task.multi) {
+          var config = grunt.config.getRaw(task.name);
+          var targets = _(config).omit(['options']).keys().map(target => `${task.name}:${target}`).value();
+          inquirer.prompt([{
+            type: 'checkbox',
+            message: 'Select target(s)',
+            name: 'targets',
+            choices: targets,
+            pageSize: calcSize(targets)
+          }]).then(answers => {
+            // If they select all, don't pass a target at all
+            if (answers.targets.length === targets.length) {
+              next(null, answer);
+            } else {
+              next(null, answers.targets);
+            }
+          });
+        } else {
+          next(null, answer);
+        }
+      }, function(err, tasksToRun) {
+        tasksToRun = _.flatten(tasksToRun);
+        inquirer.prompt([{
+          type: 'ordered',
+          message: 'Use h to move a task higher, l to move it lower',
+          name: 'order',
+          choices: tasksToRun,
+          default: tasksToRun,
+          pageSize: calcSize(tasksToRun)
+        }]).then(answers => {
+          grunt.task.run(answers.order);
+          done();
+        });
+      });
+    });
   });
 };
--- test/idk.coffee
+++ test/idk.coffee
@@ -1,2 +1,51 @@
+chalk = require('chalk')
+
 describe 'idk', ->
-  Given -> @subject = proxyquire '../tasks/idk'
+  Given -> @inquirer =
+    prompt: sinon.stub()
+  Given -> @subject = require('proxyquire').noCallThru() '../tasks/idk',
+    inquirer: @inquirer
+
+  Given -> @fakePromise =
+    then: sinon.stub()
+
+  Given -> @grunt =
+    registerTask: sinon.stub()
+    log:
+      write: sinon.stub()
+    config:
+      getRaw: sinon.stub()
+    task:
+      run: sinon.stub()
+
+  context 'no options', ->
+    context 'less than 20 tasks', ->
+      Given -> @grunt.task._tasks =
+        foo:
+          info: 'Alias for "baz" task'
+        bar:
+          info: 'Do bar'
+          multi: true
+        baz:
+          info: 'Do baz'
+      Given -> @foo = chalk.gray('foo:') + ' Alias for "baz" task'
+      Given -> @bar = chalk.gray('bar:') + ' Do bar'
+      Given -> @baz = chalk.gray('baz:') + ' Do baz'
+      Given -> @inquirer.prompt.withArgs([
+        type: 'checkbox'
+        message: 'Select task'
+        name: 'tasks'
+        choices: [@foo, @bar, @baz]
+        pageSize: 3
+      ]).returns @fakePromise
+      Given -> @fakePromise.onCall(0).callsArgWith(0, { tasks: [@foo, @bar, @baz] })
+
+
+    context 'more than 20 tasks', ->
+      context 'and no LINES', ->
+
+      context 'and LINES set', ->
+        
+    
+  context 'with options', ->
+    
